0bcda688548eccb058ad33864737acf2a4f79f40
Jillo Woche <jillo.abdulla@gmail.com>
2026-01-20 12:13:16 +0300

    chore: remove mocks for now

--- src/mock/room.ts
+++ src/mock/room.ts
@@ -1,24 +1,24 @@
-import type { RoomState } from "../types";
+// import type { RoomState } from "../types";
 
-export const mockRoom: RoomState = {
-  id: "ABC123",
-  name: "Mock room",
-  adminId: "1",
-  votingSystem: "fibonacci",
-  revealPolicy: "everyone",
-  revealed: false,
-  users: [
-    { id: "1", name: "Jay", hasVoted: false },
-    { id: "2", name: "Alex", hasVoted: false },
-    { id: "3", name: "Sam", hasVoted: false },
-    { id: "4", name: "John", hasVoted: false },
-    { id: "5", name: "Jane", hasVoted: false },
-  ],
-  votes: {
-    "1": null,
-    "2": null,
-    "3": null,
-    "4": null,
-    "5": "13",
-  },
-};
+// export const mockRoom: RoomState = {
+//   id: "ABC123",
+//   name: "Mock room",
+//   adminId: "1",
+//   votingSystem: "fibonacci",
+//   revealPolicy: "everyone",
+//   revealed: false,
+//   users: [
+//     { id: "1", name: "Jay", hasVoted: false },
+//     { id: "2", name: "Alex", hasVoted: false },
+//     { id: "3", name: "Sam", hasVoted: false },
+//     { id: "4", name: "John", hasVoted: false },
+//     { id: "5", name: "Jane", hasVoted: false },
+//   ],
+//   votes: {
+//     "1": null,
+//     "2": null,
+//     "3": null,
+//     "4": null,
+//     "5": "13",
+//   },
+// };
